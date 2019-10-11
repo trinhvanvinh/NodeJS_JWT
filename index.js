ac2eb498f3cd3ad4e0aa019e53742013f3ae431f
mac <>
2019-10-11 16:58:17 +0700

    v1

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express =  require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
-//route middelewares
 
-app.use('/api/user', authRoute);
+// connect to db
+mongoose.connect('mongodb+srv://restAPI:trinhvanvinh018@restapi-4od1g.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true },
+()=>console.log('connected to db')
+);
+
+app.use(express.json());
+
+app.use('/api', authRoute);
 
 
 app.listen(3000 ,()=> console.log('Server up and running ...'));
